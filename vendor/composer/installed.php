68e544b0a5467c827e0d62bc1dd8052fbdf19546
Nathan Conley <nateconley123@gmail.com>
2021-12-30 16:10:40 -1000

    Adds back in vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bd33cd3677669f00c6cbf9a59572a07b83bca7fe',
+    'name' => 'nateconley/nateconley-backend',
+  ),
+  'versions' => 
+  array (
+    'johnbillion/extended-cpts' => 
+    array (
+      'pretty_version' => '4.5.2',
+      'version' => '4.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '59ce9f4e0422512eec6f698ccba4a6a81b018fdb',
+    ),
+    'nateconley/nateconley-backend' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bd33cd3677669f00c6cbf9a59572a07b83bca7fe',
+    ),
+  ),
+);
